1f26fb603da03c5343dfa268f4bd417e0abaebd6
Héctor Navarro Martín <h.navarro@oan.es>
2010-06-22 17:00:18 +0200

    Done with Sign in proccess

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ ActionController::Routing::Routes.draw do |map|
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   map.resources :products
   map.resources :users
+  map.resources :sessions, :only => [:new, :create, :destroy]
 
   # Sample resource route with options:
   #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
@@ -45,4 +46,6 @@ ActionController::Routing::Routes.draw do |map|
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.signup 'signup', :controller => 'users', :action => 'new'
+  map.signin 'signin', :controller => 'sessions', :action => 'new'
+  map.signout 'signout', :controller => 'sessions', :action => 'destroy'
 end
